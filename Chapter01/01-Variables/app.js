b3123e960dd135c1ee59170ca72bec3069e96538
owenmartin-bd <owen_0918@live.com>
2020-04-16 21:42:49 +0600

    First commit

---
+++ Chapter01/01-Variables/app.js
@@ -0,0 +1,14 @@
+var num = 1; //{1}
+num = 3; //{2}
+var price = 1.5; //{3}
+var name = 'Packt'; //{4}
+var trueValue = true; //{5}
+var nullVar = null; //{6}
+var und;
+
+console.log("num: "+ num);
+console.log("name: "+ name);
+console.log("trueValue: "+ trueValue);
+console.log("price: "+ price);
+console.log("nullVar: "+ nullVar);
+console.log("und: "+ und);
---
+++ Chapter01/03-Operators/app.js
@@ -0,0 +1,45 @@
+var num = 0; // {1}
+num = num + 2;
+num = num * 3;
+num = num / 2;
+num++;
+num--;
+
+num += 1; // {2}
+num -= 2;
+num *= 3;
+num /= 2;
+num %= 3;
+
+console.log('num == 1 : ' + (num == 1)); //{3}
+console.log('num === 1 : ' + (num === 1));
+console.log('num != 1 : ' + (num != 1));
+console.log('num > 1 : ' + (num > 1));
+console.log('num < 1 : ' + (num < 1));
+console.log('num >= 1 : ' + (num >= 1));
+console.log('num <= 1 : ' + (num <= 1));
+
+console.log('true && false : ' + (true && false)); // {4}
+console.log('true || false : ' + (true || false));
+console.log('!true : ' + (!true));
+
+// bitwise operators
+console.log('5 & 1:', (5 & 1));
+console.log('5 | 1:', (5 | 1));
+console.log('~ 5:', (~5));
+console.log('5 ^ 1:', (5 ^ 1));
+console.log('5 << 1:', (5 << 1));
+console.log('5 >> 1:', (5 >> 1));
+
+
+// typeof operator
+console.log('typeof num:', typeof num);
+console.log('typeof Packt:', typeof 'Packt');
+console.log('typeof true:', typeof true);
+console.log('typeof [1,2,3]:', typeof [1,2,3]);
+console.log('typeof {name:John}:', typeof {name:'John'});
+
+// delete operator
+var myObj = {name: 'John', age: 21};
+delete myObj.age;
+console.log(myObj); //outputs Object {name: "John"}
