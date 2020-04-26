8ff3267d1dddbe22b74c748738e70cf7cf8b568c
owenmartin-bd <owen_0918@live.com>
2020-04-26 10:37:51 +0600

    Sets

--- Chapter05/02-doubly-linked-list/app.js
+++ Chapter05/02-doubly-linked-list/app.js
@@ -2,10 +2,91 @@ function DoublyLinkedList() {
     let Node = function(element){
         this.element = element;
         this.next = null;
-        this.prev = null;
+        this.prev = null; // NEW
     };
 
     let length = 0;
     let head = null;
-    let tail = null;
+    let tail = null; // NEW
+
+    this.insert = function(position, element) {
+
+        // check for out-of-bound values
+        if(position >= 0 && position <= length) {
+            let node = new Node(element),
+            current = head,
+            previous,
+            index = 0;
+
+            if(position === 0){
+                if(!head) { // {1}
+                    head = node;
+                    tail = node;
+                } else {
+                    node.next = current;
+                    current.prev = node; // NEW {2}
+                    head = node;
+                }
+            } else if(position === length) {
+                current = tail; // {3}
+                current.next = node;
+                node.prev = current;
+                tail = node;
+            } else {
+                while(index ++ < position) { // {4}
+                    previous = current;
+                    current = current.next;
+                }
+                node.next = current; // {5}
+                previous.next = node;
+
+                current.prev = node; // NEW
+                node.prev = previous; // NEW
+            }
+            length++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    this.removeAt = function(position) {
+        
+        // look for out-of-bound values
+        if(position > -1 && position < length){
+
+            let current = head,
+            previous,
+            index = 0;
+
+            // removing first item
+            if(position === 0){
+                head = current.next; // {1}
+
+                // if there is only one item, update tail //NEW
+                if(length === 1){ // {2}
+                    tail = null;
+                } else {
+                    head.prev = null // {3}
+                }
+            } else if (position === length-1){ // last item // NEW
+                current = tail; // {4}
+                tail = current.prev;
+                tail.next = null;
+            } else {
+                while(index++ < position) { // {5}
+                    previous = current;
+                    current = current.next;
+                }
+
+                // link previous with current's next - skip it
+                previous.next = current.next; // {6}
+                current.next.prev = previous; // NEW
+            }
+            length--;
+            return current.element
+        } else {
+            return null;
+        }
+    }
 }
---
+++ Chapter06/01-set-basics/app.js
@@ -0,0 +1,93 @@
+function Set() {
+    let items = {};
+
+    // The has (value) method
+    this.has = function(value) {
+        // return value in items;
+        return items.hasOwnProperty(value); // better approach than the previous code
+    }
+
+    // the add method
+    this.add = function(value){
+        if(!this.has(value)){
+            items[value] = value; // {1}
+            return true;
+        }
+        return false;
+    }
+
+    // the delete and clear methods
+    this.delete = function(value) {
+        if(this.has(value)){
+            delete items[value]; //{2}
+            return true;
+        }
+        return false;
+    }
+
+    this.clear = function() {
+        items = {};
+    }
+
+    // The size method
+    this.size = function() { // Only supports mordern browsers
+        return Object.keys(items).length; // {4}
+    }
+
+    this.sizeLegacy = function() {
+        let count = 0;
+        for(let key in items){ // {5}
+            if(items.hasOwnProperty(key)) { // {6} 
+                ++count; // {7}
+            }
+            return count;
+        }
+    }
+
+    // The values method
+    this.values = function() {
+        let values = [];
+        for(let i=0, keys=Object.keys(items); i < keys.length; i++) {
+            values.push(items[keys[i]]);
+        }
+        return values;
+    }
+
+    this.valuesLegacy = function() {
+        let values = [];
+        for(let key in items) { //{7}
+            if(items.hasOwnProperty(key)) { // {8}
+                values.push(items[key])
+            }
+        }
+        return values;
+    }
+
+    this.union = function(otherSet) {
+        let unionSet = new Set(); // {1}
+
+        let values = this.values(); // {2}
+        for(let i = 0; i < values.length; i++) {
+            unionSet.add(values[i]);
+        }
+
+        values = otherSet.values(); // {3}
+        for(let i = 0; i < values.length; i++) {
+            unionSet.add(values[i]);
+        }
+
+        return unionSet;
+    }
+}
+
+let set = new Set();
+
+set.add(1);
+console.log(set.values());
+console.log(set.has(1));
+console.log(set.size());
+
+set.add(2);
+console.log(set.values());
+console.log(set.has(2));
+console.log(set.size());
